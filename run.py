af5c2fc77428c13f2bbd7b189c0cfbd88772d514
FaridBenamara <f_benamara@hetic.eu>
2024-09-05 02:58:18 +0200

    Correct requirements.txt & Adding CORS Policy

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 from app import create_app
-from app.mqtt_broker import subscribe_to_topic
-from app import mqtt_client
+from flask_cors import CORS  # Importer CORS
 import os
 
 app = create_app()
 
+# Configurer CORS
+CORS(app, resources={r"/*": {"origins": "*"}})  # Permet toutes les origines. Ajustez selon vos besoins.
+
 if __name__ == '__main__':
     # Subscribe to MQTT topics
-    
-    subscribe_to_topic(mqtt_client, "esp32/#")  # Subscribe to all topics under "esp32"
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    from app import mqtt_client
+    from app.mqtt_broker import subscribe_to_topic
+
+    subscribe_to_topic(mqtt_client, "esp32bis/#")  # Subscribe to all topics under "esp32Bis"
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
